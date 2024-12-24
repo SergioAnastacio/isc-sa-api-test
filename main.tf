dd16fe918cbb57f4203b855edec85c325ddb3acc
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-23 22:04:16 -0600

    add aviavility_zones

--- main.tf
+++ main.tf
@@ -22,16 +22,16 @@ resource "aws_internet_gateway" "anasty_gateway" {
   vpc_id = aws_vpc.anasty_vpc.id
 }
 
-resource "aws_subnet" "anasty_subnet" {
-  vpc_id     = aws_vpc.anasty_vpc.id
-  cidr_block = "172.30.0.0/24"
+resource "aws_subnet" "anasty_subnet_a" {
+  vpc_id            = aws_vpc.anasty_vpc.id
+  cidr_block        = "172.30.0.0/24"
+  availability_zone = "us-west-1a"
 }
 
-resource "aws_subnet" "anasty_subnet_ipv6" {
+resource "aws_subnet" "anasty_subnet_b" {
   vpc_id            = aws_vpc.anasty_vpc.id
   cidr_block        = "172.30.1.0/24"
   availability_zone = "us-west-1c"
-  ipv6_cidr_block   = cidrsubnet(aws_vpc.anasty_vpc.ipv6_cidr_block, 8, 1)
 }
 
 resource "aws_route_table" "anasty_route_table" {
@@ -44,46 +44,14 @@ resource "aws_route" "default_route" {
   gateway_id             = aws_internet_gateway.anasty_gateway.id
 }
 
-resource "aws_route_table_association" "anasty_table_association" {
-  subnet_id      = aws_subnet.anasty_subnet.id
+resource "aws_route_table_association" "anasty_table_association_a" {
+  subnet_id      = aws_subnet.anasty_subnet_a.id
   route_table_id = aws_route_table.anasty_route_table.id
 }
 
-resource "aws_route_table_association" "anasty_table_association_ipv6" {
-  subnet_id      = aws_subnet.anasty_subnet_ipv6.id
+resource "aws_route_table_association" "anasty_table_association_b" {
+  subnet_id      = aws_subnet.anasty_subnet_b.id
   route_table_id = aws_route_table.anasty_route_table.id
-}
-
-resource "aws_security_group" "anasty_security_group" {
-  vpc_id = aws_vpc.anasty_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_instance" "back_anasty" {
@@ -91,7 +59,7 @@ resource "aws_instance" "back_anasty" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.anasty_security_group.id]
   key_name               = "masternode"
-  subnet_id              = aws_subnet.anasty_subnet.id
+  subnet_id              = aws_subnet.anasty_subnet_a.id
   associate_public_ip_address = true
 }
 
@@ -107,7 +75,6 @@ resource "local_file" "instance_ip_file" {
   content  = aws_eip.back_anasty_eip.public_ip
   filename = "${path.module}/instance_ip.txt"
 }
-
 
 resource "aws_db_instance" "anasty_db" {
   allocated_storage    = 20
@@ -126,7 +93,7 @@ resource "aws_db_instance" "anasty_db" {
 
 resource "aws_db_subnet_group" "anasty_db_subnet_group" {
   name       = "anasty-db-subnet-group"
-  subnet_ids = [aws_subnet.anasty_subnet.id, aws_subnet.anasty_subnet_ipv6.id]
+  subnet_ids = [aws_subnet.anasty_subnet_a.id, aws_subnet.anasty_subnet_b.id]
 }
 
 resource "aws_security_group" "anasty_db_sg" {
